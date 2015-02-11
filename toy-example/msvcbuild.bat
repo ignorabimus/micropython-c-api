9260b31cdea6ecc4f829b9a846c1b3045308e9af
Tatsuya Watanabe <watanabe@ignorabimus.info>
2015-02-11 23:35:58 +0900

    Toy example of the C API in Micro Python

---
+++ toy-example/msvcbuild.bat
@@ -0,0 +1,4 @@
+call "%VS120COMNTOOLS%vsvars32.bat"
+
+msbuild toy-example.vcxproj /p:configuration=release /p:platform=win32
+msbuild toy-example.vcxproj /p:configuration=debug /p:platform=win32
---
+++ toy-example/toy-example.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+
+#ifdef _DEBUG
+#pragma comment(lib, "micropython_d.lib")
+#else
+#pragma comment(lib, "micropython.lib")
+#endif
+
+#include "py/parsehelper.h"
+#include "py/runtime.h"
+#include "py/gc.h"
+
+int execute_from_lexer(mp_lexer_t *lex, mp_parse_input_kind_t input_kind, bool is_repl);
+int handle_uncaught_exception(mp_obj_t exc);
+
+bool do_string(const char *str)
+{
+	mp_lexer_t *lex = mp_lexer_new_from_str_len(MP_QSTR__lt_stdin_gt_, str, strlen(str), false);
+	return execute_from_lexer(lex, MP_PARSE_SINGLE_INPUT, false) == 0;
+}
+
+mp_obj_t callback_print(mp_obj_t arg)
+{
+	const char *str = mp_obj_str_get_str(arg);
+	printf("c_print: %s\n", str);
+
+	return mp_const_none;
+}
+
+mp_int_t call_add1(mp_int_t n)
+{
+	mp_int_t ret;
+
+	nlr_buf_t nlr;
+	if (nlr_push(&nlr) == 0) {
+		mp_obj_t fun = mp_load_name(qstr_from_str("add1"));
+		mp_obj_t arg = mp_obj_new_int(n);
+		mp_obj_t o = mp_call_function_1(fun, arg);
+
+		ret = mp_obj_get_int(o);
+
+		nlr_pop();
+	} else {
+		handle_uncaught_exception((mp_obj_t)nlr.ret_val);
+
+		return false;
+	}
+
+	return ret;
+}
+
+int main(void) {
+	const long heap_size = 128 * 1024 * (sizeof(mp_uint_t) / 4);
+
+	char *heap = (char *)malloc(heap_size);
+	gc_init(heap, heap + heap_size);
+	mp_init();
+
+	// call a C function from Python
+	mp_store_name(qstr_from_str("c_print"), mp_obj_new_fun_native(1, callback_print));
+	do_string("c_print('Hello world!')\n");
+
+	// call a Python function from C
+	do_string("add1 = lambda x:x+1\n");
+	printf("call_add1: %d\n", call_add1(10));
+
+    mp_deinit();
+	free(heap);
+
+    return 0;
+}
